a85bb421d3e81345b94da2d1eebc90a8726b150c
Marcelo Cesar <marcelo.cesards@gmail.com>
2020-08-31 21:07:16 -0300

    aula 4 online

---
+++ index.js
@@ -0,0 +1,8 @@
+let inputValorEntrada = document.getElementById("valorEntrada");
+console.log(inputValorEntrada);
+let segundoInput = document.getElementById("segundoValor");
+console.log(segundoInput);
+let textAreaResultado = document.getElementById("resultado");
+console.log(textAreaResultado);
+let botaoCalcular = document.getElementById("botaoCalcular");
+console.log(botaoCalcular);
