e82273a83d4c6b76a58db146017ef0fc2073b3ea
Marcelo <marcelo.cesards@gmail.com>
2020-09-21 21:03:15 -0300

    Iniciando aula de calculos numéricos

--- public/index.js
+++ public/index.js
@@ -4,9 +4,11 @@ let variavelParaGuardarOBotao = document.getElementById("botao");
 console.log(variavelParaGuardarOBotao);
 let textAreaResultado = document.getElementById("textAreaResultado");
 console.log(textAreaResultado);
+let segundoInput = document.getElementById("inputSegundoValor");
 
 variavelParaGuardarOBotao.addEventListener("click", function(){
     console.log(inputText.value);   
-    textAreaResultado.textContent = textAreaResultado.textContent +"\n"+ inputText.value;
+    let resultado = parseFloat(inputText.value) + parseFloat(segundoInput.value);
+    textAreaResultado.textContent = textAreaResultado.textContent  +"\n"+ resultado;
     console.log(1 + 1);
 });
