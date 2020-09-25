f43fce818c6574b856e6b3924ba670851cbeae83
Marcelo <marcelo.cesards@gmail.com>
2020-09-25 18:48:30 -0300

    Aula 5 soma

--- public/index.js
+++ public/index.js
@@ -4,10 +4,10 @@ let variavelParaGuardarOBotao = document.getElementById("botao");
 console.log(variavelParaGuardarOBotao);
 let textAreaResultado = document.getElementById("textAreaResultado");
 console.log(textAreaResultado);
-let segundoInput = document.getElementById("inputSegundoValor");
+let segundoInput = document.getElementById("segundoValor");
 
 variavelParaGuardarOBotao.addEventListener("click", function(){
-    console.log(inputText.value);   
+    console.log(inputText.value);  
     let resultado = parseFloat(inputText.value) + parseFloat(segundoInput.value);
     textAreaResultado.textContent = textAreaResultado.textContent  +"\n"+ resultado;
     console.log(1 + 1);
